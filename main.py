880158727bda50ba8460ee0ba47607dce12acc8d
Nicolas <nicolasmader001@gmail.com>
2026-03-07 18:42:47 +0100

    Sonderzeichen und Passwortlänge hinzugefügt

--- main.py
+++ main.py
@@ -4,36 +4,54 @@ def password_generator():
     choice = None
 
     while choice not in ["1","2","3","4"]:
-        print("1 : Passwort Generiren")
+        print("1 : Passwort Generieren")
         print("2 : Wählen sie länge des Passwortes")
         print("3 : Wollen sie ein Sonderzeichen?")
         print("4 : Beenden")
 
         choice = input("Wähle eine Option:")
-        print("Du häst gewählt:", choice)
+        print("Du hast gewählt:", choice)
 
         if choice not in ["1","2","3","4"]:
             print("Ungültige Option, bitte erneut zwischen 1,2,3,4  wählen.")
 
     return choice
 
-def password_laeng():
+def password_laenge():
         print("Wie lang soll ihr password sein")
-        laeng = int(input("Länge:"))
-        return laeng
+        laenge = int(input("Länge:"))
+        return laenge
 
-def password_generator_code():
+
+def password_generator_code(leange):
+
+    buchstaben_liste = ["A","B","C","D","E","F","G","H","I","J","K","L",
+                        "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                        "a","b","c","d","e","f","g","h","i","j","k","l","m",
+                        "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+    if sonderzeichen == "y":
+        alle_zeichen = buchstaben_liste + password_sonderzeichen()
+
+    else :
+        alle_zeichen = buchstaben_liste
     password_list = []
-    password_list.append("A")
-    password_list.append("B")
-    password_list.append("C")
+
+    for i in range(leange):
+        password_list.append(alle_zeichen[i % len(alle_zeichen)])
+
     password = "".join(password_list)
-    print(password)
+    print("Dein Passwort:",password)
 
-#start werte ändern sich
 
-leang = 8
+def password_sonderzeichen():
+    sonderzeichen_list = ["!", "?", "#", "$", "&"]
+    return sonderzeichen_list
 
+
+#start
+leange = 8
+sonderzeichen = ""
 
 while True:
     choice = password_generator()
@@ -41,20 +59,24 @@ while True:
     match choice :
 
         case "1" :
-            print("Passwort Generiren")
-            password_generator_code()
+            print("Passwort Generieren")
+            password_generator_code(leange)
+
 
         case "2" :
             print("Wählen sie länge des Passwortes")
-            leang = password_laeng()
-            print("Dein Passwort soll ", leang,"Zeichen sein"
-                  )
+            leange = password_laenge()
+            print("Dein Passwort soll ", leange,"Zeichen sein")
+
+
         case "3":
             print("Wollen sie ein Sonderzeichen?")
             sonderzeichen = input("Wollen sie das Passwort mit sonderzeichen? (y/n):")
 
+
         case "4":
             print("Beenden")
             break
+
 
 
